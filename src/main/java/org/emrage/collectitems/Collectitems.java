95f4b5f7e230e60b9ab31088be55c81f0050661a
GamerPvG <gamerghg2507@gmail.com>
2023-11-08 22:58:07 +0100

    Initial commit

---
+++ src/main/java/org/emrage/collectitems/Collectitems.java
@@ -0,0 +1,17 @@
+package org.emrage.collectitems;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Collectitems extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
